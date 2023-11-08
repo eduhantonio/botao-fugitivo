4908c0a8c93a65f89abc5bf477682ebfc370edfd
Eduardo Antonio <126982066+eduhantonio@users.noreply.github.com>
2023-11-08 13:42:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+// Clicar no Botão "Não"
+
+const botaoNao = document.getElementById("botao-nao")
+const OFFSET = 100
+
+botaoNao.addEventListener('click', () => {
+    alert("LAMENTÁVEL NOVINHA ZZ")
+})
+
+// Identifica a Posição do Mouse
+
+document.addEventListener('mousemove', (e) => {
+    const x = e.pageX
+    const y = e.pageY
+    const buttonBox = botaoNao.getBoundingClientRect()
+    
+    const horizontalDistanceFrom = distanceFromCenter(buttonBox.x, x, buttonBox.width)
+    const verticalDistanceFrom = distanceFromCenter(buttonBox.y, y, buttonBox.height)
+    
+    const horizontalOffset = buttonBox.width / 2 + OFFSET
+    const verticalOffset = buttonBox.height / 2 + OFFSET
+    
+    if (Math.abs(horizontalDistanceFrom) <= horizontalOffset && Math.abs(verticalDistanceFrom) <= verticalOffset){
+        setButtonPosition(
+            buttonBox.x + horizontalOffset / horizontalDistanceFrom * 10, 
+            buttonBox.x + verticalOffset / verticalDistanceFrom * 10,
+            )
+    }
+})
+
+// Funções de Código
+
+function setButtonPosition(left, top) {
+    const windowBox = document.body.getBoundingClientRect()
+    const buttonBox = botaoNao.getBoundingClientRect()
+  
+    if(distanceFromCenter(left, windowBox.left, buttonBox.width) < 0) {
+      left = windowBox.right - buttonBox.width - OFFSET
+    }
+    if(distanceFromCenter(left, windowBox.right, buttonBox.width) > 0) {
+      left = windowBox.left + OFFSET
+    }
+    if(distanceFromCenter(top, windowBox.top, buttonBox.height) < 0) {
+      top = windowBox.bottom - buttonBox.height - OFFSET
+    }
+    if(distanceFromCenter(top, windowBox.bottom, buttonBox.height) > 0) {
+      top = windowBox.top + OFFSET
+    }
+  
+    botaoNao.style.left = `${left}px`
+    botaoNao.style.top = `${top}px`
+  }
+
+function distanceFromCenter(boxPosition, mousePosition, boxSize) {
+    return boxPosition - mousePosition + boxSize / 2
+}
